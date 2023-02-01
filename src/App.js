4c88d5324cce49ca100fc386864da3d0da1d3fe2
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-02-01 11:28:33 +0530

    folder structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Component/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Sidebar/>
     </div>
   );
 }
